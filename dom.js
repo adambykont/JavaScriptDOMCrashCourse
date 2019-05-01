a75460b4146fc902ce1936e8e5ee8dd5a52f7c4e
Janusz <JanuszAGH@users.noreply.github.com>
2019-05-01 12:57:40 +0200

    listeners (#1)
    * listeners
    
    * change title when clicked

--- dom.js
+++ dom.js
@@ -1,1 +1,17 @@
+let btn = document.querySelector('#button');
 
+let counter = 0;
+
+function btnCallback() {
+    let title = document.querySelector('#header-title');
+    let msg;
+    if (counter % 2 === 0) {
+        msg = counter;
+    } else {
+        msg = new Date().getHours();
+    }
+    counter++;
+    title.textContent = `Clicked ${msg} times`;
+}
+
+btn.addEventListener('click', btnCallback);
