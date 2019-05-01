ff9efeaabe06ab290750a1172467346b8f1b563e
janusz <janusz.agh@gmail.com>
2019-05-01 15:44:41 +0200

    event with coordinates revisited

--- dom.js
+++ dom.js
@@ -1,9 +1,15 @@
 let btn = document.querySelector('#button');
 
-let counter = 0;
+function showTarget(event) {
+    console.log(event.target);
+}
+
+document.body.addEventListener('click', showTarget);
 
 function btnCallback(e) {
     console.log(`${e.x} : ${e.y}`);
+    console.log(`${e.clientX} : ${e.clientY}`);
+    // console.log(e.target);
 }
 
 btn.addEventListener('click', btnCallback);
