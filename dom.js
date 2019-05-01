350d308e4d01af5d9ee39a3a38aadef90e8680f3
janusz <janusz.agh@gmail.com>
2019-05-01 14:47:57 +0200

    entire event

--- dom.js
+++ dom.js
@@ -2,16 +2,8 @@ let btn = document.querySelector('#button');
 
 let counter = 0;
 
-function btnCallback() {
-    let title = document.querySelector('#header-title');
-    let msg;
-    if (counter % 2 === 0) {
-        msg = counter;
-    } else {
-        msg = new Date().getHours();
-    }
-    counter++;
-    title.textContent = `Clicked ${msg} times`;
+function btnCallback(e) {
+    console.log(e);
 }
 
 btn.addEventListener('click', btnCallback);
