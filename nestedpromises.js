6d8e11993cfc819b2c01e200c77706f64f327196
janusz <janusz.agh@gmail.com>
2019-05-22 22:22:19 +0200

    single thread

--- nestedpromises.js
+++ nestedpromises.js
@@ -1,1 +1,28 @@
-console.log('cleaned up')
+const cleanRoom = function () {
+    return new Promise(function (resolve, reject) {
+        resolve("Cleaned the room");
+    })
+};
+
+const removeGarbage = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve(message + '. Garbages has been removed');
+    });
+}
+
+const winIcecream = function (message) {
+    return new Promise(function (resolve, reject) {
+        resolve(message + '. Won icecream.');
+    });
+};
+
+cleanRoom()
+    .then(function (result) {
+        return removeGarbage(result);
+    })
+    .then(function (result) {
+        return winIcecream(result);
+    })
+    .then(function (result) {
+        console.log("Finished: " + result);
+    });
