3e83cb86727cc87e6769b9f7ede2847f90cf88de
janusz <janusz.agh@gmail.com>
2019-05-22 20:51:42 +0200

    cleaned up

--- nestedpromises.js
+++ nestedpromises.js
@@ -1,15 +1,1 @@
-function cleanTheRoom(resolve, reject) {
-    const isClean = Math.random() > 0.5;
-    if (isClean) {
-        resolve('Room is clean');
-    } else {
-        reject('Still messy');
-    }
-}
-
-const dailyJob = new Promise(cleanTheRoom);
-
-dailyJob
-    .then((fromResolve) => console.log("Result: " + fromResolve))
-    .catch(fromReject => console.log("Result: " + fromReject))
-    .finally(() => console.log("That's all for today"));
+console.log('cleaned up')
